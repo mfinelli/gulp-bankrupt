dc47ca02b90345925c873fc992405ee067b6a1d7
Mario Finelli <mario@finel.li>
2017-04-29 14:11:11 -0400

    Add initial index
    Currently only calculates md5 hashes for the given files.

---
+++ index.js
@@ -0,0 +1,46 @@
+var crypto = require('crypto'),
+  gutil = require('gulp-util'),
+  path = require('path'),
+  through = require('through2');
+
+const PLUGIN_NAME = 'gulp-bankrupt',
+  DEFAULTS = {
+    fileName: 'manifest.json',
+    hashAlgorithm: 'md5',
+    sriAlgorithm: 'sha384'
+  }
+
+var manifest = {};
+
+module.exports = function() {
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+
+    if (file.isBuffer()) {
+      manifest[file.path] = crypto.createHash(DEFAULTS.hashAlgorithm)
+        .update(file.contents).digest('hex');
+    }
+
+    if (file.isStream()) {
+      var hash = crypto.createHash(DEFAULTS.hashAlgorithm);
+      hash.setEncoding('hex');
+
+      file.on('end', function() {
+        hash.end();
+        manifest[file.path] = hash.read();
+      });
+
+      file.pipe(hash);
+    }
+
+    cb(null, file);
+  }, function(cb) {
+    this.push(new gutil.File({
+      path: path.join(process.cwd(), DEFAULTS.fileName),
+      contents: new Buffer(JSON.stringify(manifest))
+    }));
+    cb();
+  });
+};
